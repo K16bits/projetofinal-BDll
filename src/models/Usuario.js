4ccbdfaa29def22c660a6c225511857298acb9d0
Cristiano <cristiano.16bits@gmail.com>
2022-07-30 16:21:36 -0300

    controle de acesso,adm modifica usuario

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const UsuarioSchema = new Schema(
-  {
-    nome: String,
-    tipo: String,
-    senha: String,
-    email: String
-  }
-);
+const UsuarioSchema = new Schema({
+  nome: String,
+  documento: String,
+  telefone: String,
+  dataNascimento: String,
+  endRua: String,
+  endNumero: String,
+  endComplemento: String,
+  endCep: String,
+  endBairro: String,
+  endCidade: String,
+  endEstado: String,
+  endPais: String,
+  email: String,
+  senha: String,
+  tipo: String,
+});
 
 const Usuario = model("Usuario", UsuarioSchema);
-module.exports = Usuario
+module.exports = Usuario;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,62 +1,108 @@
-const Evento = require('../models/Evento.js')
-const Usuario = require('../models/Usuario.js')
+const Evento = require("../models/Evento.js");
+const Usuario = require("../models/Usuario.js");
 
-const { Router } = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
 //--------------------- CRUD  --------------------
 
-router.post('/criar-usuario',async (req,res)=>{
-  const {nome,senha,tipo,email} = req.body;
-  const user = await Usuario.create({nome,senha,tipo:"adm",email})
-  res.render('login')
-})
+router.post("/criar-usuario", async (req, res) => {
+  const {
+    nome,
+    documento,
+    telefone,
+    dataNascimento,
+    endRua,
+    endNumero,
+    endComplemento,
+    endCep,
+    endBairro,
+    endCidade,
+    endEstado,
+    endPais,
+    email,
+    senha,
+  } = req.body;
 
-router.post('/login',async (req,res)=>{
-  const {email,senha} = req.body;
+  const user = await Usuario.create({
+    nome,
+    documento,
+    telefone,
+    dataNascimento,
+    endRua,
+    endNumero,
+    endComplemento,
+    endCep,
+    endBairro,
+    endCidade,
+    endEstado,
+    endPais,
+    email,
+    senha,
+    tipo: "adm",
+  });
+  res.render("login");
+});
+
+router.post("/login", async (req, res) => {
+  const { email, senha } = req.body;
   // console.log(email)
-  const user = await Usuario.findOne({email}).lean()
-  console.log(user)
-  if(!user){
-    return res.status(400).json({msg:"Usuario inválido"})
+  const user = await Usuario.findOne({ email }).lean();
+  console.log(user);
+  if (!user) {
+    return res.status(400).json({ msg: "Usuario inválido" });
   }
 
-  if(senha===user.senha){
-    if(user.tipo === 'adm'){return res.redirect('/adm')}  
-    return res.redirect('/user')
+  if (senha === user.senha) {
+    if (user.tipo === "adm") {
+      return res.redirect("/adm");
+    }else if(user.tipo === "fiscal"){
+      return res.redirect("/fiscal");
+    }
+    return res.redirect("/user");
+  
   }
+  
+  return res.status(400).json({ msg: "Usuario e/ou senha inválida" });
+});
 
-  return res.status(400).json({msg:"Usuario e/ou senha inválida"})
-})
+router.post("/criar-evento", async (req, res) => {
+  const { nome } = req.body;
+  const eventoData = await Evento.create({ nome, status: false });
+  res.redirect("/adm");
+});
 
-router.post('/criar-evento', async(req, res) => {
-  const {nome} = req.body
-  const eventoData = await Evento.create({nome,status:false})
-  res.redirect('/adm')
-})
+router.post("/adionar-no-evento/_id", async (req, res) => {
+  const { nome } = req.body;
+  const eventoData = await Evento.findByIdAndUpdate(_id, {
+    $push: { participantes: { nome: nome } },
+  });
+  res.status(200).json({ data: eventoData });
+});
 
-router.post('/adionar-no-evento/_id', async(req, res) => {
-  const {nome} = req.body
-  const eventoData = await Evento.findByIdAndUpdate(_id,{ $push:{participantes:{nome:nome}} })
-  res.status(200).json({data:eventoData})
-})
-
-router.post('/criar-atividade', async(req, res) => {
-  const {idEvento,nomeAtividade} = req.body
+router.post("/criar-atividade", async (req, res) => {
+  const { idEvento, nomeAtividade } = req.body;
   // console.log(idEvento,nomeAtividade)
-  const data = await Evento.findByIdAndUpdate({_id:idEvento},{ $push:{atividades:{nome:nomeAtividade}} })
-  res.redirect(`/adm/gerenciar_evento?_idEvento=${idEvento}`)
-})
+  const data = await Evento.findByIdAndUpdate(
+    { _id: idEvento },
+    { $push: { atividades: { nome: nomeAtividade } } }
+  );
+  res.redirect(`/adm/gerenciar_evento?_idEvento=${idEvento}`);
+});
 
-router.post('/adicionar-na-atividade', async(req, res) => {
-  const {_id,nome} = req.body
-  const data = await Evento.findByIdAndUpdate(_id,{ $push:{atividades:{nome:nome}} })
-  res.status(200).json({data})
-})
+router.post("/adicionar-na-atividade", async (req, res) => {
+  const { _id, nome } = req.body;
+  const data = await Evento.findByIdAndUpdate(_id, {
+    $push: { atividades: { nome: nome } },
+  });
+  res.status(200).json({ data });
+});
 
-router.post('/modificar_acesso', async(req, res) => {
-  const {email,tipo} = req.body
-  console.log(req.body)
-})
+router.post("/modificar_acesso", async (req, res) => {
+  const { email, tipo } = req.body;
+  const data = await Usuario.findOneAndUpdate({ email }, { $set: { tipo } },{new: true});
+  console.log(data)
+  res.redirect("/adm/gerenciar_fiscal")
+});
 
-module.exports = router
+module.exports = router;
